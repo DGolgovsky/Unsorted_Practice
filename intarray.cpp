e329c531c157cc0e6432fc2727a3eb9c329c2288
DGolgovsky <d.westcoast@aol.com>
2015-10-09 10:04:24 +0300

    resize method ref.

--- intarray.cpp
+++ intarray.cpp
@@ -2,13 +2,19 @@
 
 void IntArray::resize(size_t nsize)
 {
-	int * ndata = new int[nsize];
-	size_t n = nsize > (unsigned)this->data_[0] ? this->data_[0] : nsize;
-	for (size_t i = 0; i != n; ++i)
-		ndata[i] = data_[i];
-	delete [] data_;
-	data_ = ndata;
-	this->data_[0] = nsize;
+	/* Old realization
+	 * int * ndata = new int[nsize];
+	 * size_t n = nsize > (unsigned)this->data_[0] ? this->data_[0] : nsize;
+	 * for (size_t i = 0; i != n; ++i)
+	 *     ndata[i] = data_[i];
+	 * delete [] data_;
+	 * data_ = ndata;
+	 * this->data_[0] = nsize;*/
+	 IntArray t (nsize);
+	 size_t n = nsize > size_ ? size_ : nsize;
+	 for (size_t i = 0; i != n; ++i)
+		t.data_[i] = data_[i];
+	swap(t);
 }
 
 void IntArray::swap(IntArray &a)
