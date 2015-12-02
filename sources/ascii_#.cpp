2fcfa8c632bbf511a7f9862715b1e9f5a36a9329
Dmitry Golgovsky <d.westcoast@aol.com>
2015-12-02 22:57:26 +0300

    add some files

--- sources/ascii_#.cpp
+++ sources/ascii_#.cpp
@@ -5,39 +5,42 @@
 
 using namespace std;
 
-/**
+/*
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
- **/
+*/
+
 int main()
 {
-    int L;
+    int L, H, i;
     cin >> L; cin.ignore();
-    int H;
     cin >> H; cin.ignore();
     string T;
     getline(cin, T);
     string arr[H];
-    for (int i = 0; i < H; i++) {
+    for (i = 0; i < H; ++i) {
         string ROW;
         getline(cin, ROW);
         arr[i] = ROW;
     }
     string out_text[H];
-    int i = 0;
+    i = 0;
     
     while (T[i] != '\0') { //word
         int a = toupper(T[i]);
-        if (a < 65 || a > 90) a = 91;
-        for (int k = 0; k < H; k++) { //lines
-            for (int z = 0; z < L; z++) //chars
+        if (a < 65 || a > 90)
+            a = 91;
+        for (int k = 0; k < H; ++k) { //lines
+            for (int z = 0; z < L; ++z) //chars
                 out_text[k] += arr[k][(a-65)*L + z];
         }
-        i++;
+        ++i;
     }
-    for (int i = 0; i < H; i++) {
+    
+    for (i = 0; i < H; i++) {
         cout << out_text[i] << endl;
     }
     // Write an action using cout. DON'T FORGET THE "<< endl"
     // To debug: cerr << "Debug messages..." << endl;
+    return 0;
 }
