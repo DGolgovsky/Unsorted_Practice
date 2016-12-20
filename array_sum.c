3d05c59fcb877ebc555abf877160ebeec847bf05
Dmitry Golgovsky <d.westcoast@aol.com>
2016-12-20 08:34:40 +0300

    Dice game

---
+++ array_sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int array[10] = {0};
+    for (int i = 0; i < 1000; ++i)
+        for (int j = 1; j < 10; ++j)
+            if (i % j == 0)
+                array[j]++;
+
+    for (int i = 0; i < 10; ++i)
+        printf(" %3d", array[i]);
+
+    return 0;
+}
+
