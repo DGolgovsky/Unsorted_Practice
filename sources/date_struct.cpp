5c21b682f30cf493c0da3b5f12544347af880165
Dmitry Golgovsky <d.westcoast@aol.com>
2017-01-11 14:49:46 +0300

    Reformat code.

--- sources/date_struct.cpp
+++ sources/date_struct.cpp
@@ -3,32 +3,32 @@
 using namespace std;
 
 struct date
-	{
-		int day, month, year;
-		char s;
-		date()
-		{
-			day = 0;
-			month = 0;
-			year = 0;
-		}
-	};
+{
+	int day, month, year;
+	char s;
+	
+	date() :
+	  day (0),
+	  month (0),
+	  year (0);
+	{ }
+};
 
-int main()
-	{
-		date d1;				
-		cout << "Enter date 31/12/2015: ";
-		cin >> d1.day
-			>> d1.s
-			>> d1.month
-			>> d1.s
-			>> d1.year;
-		cout << endl;
-		cout << d1.day
-			 << '/'
-			 << d1.month
-			 << '/'
-			 << d1.year;
-		cout << endl;
-		return 0;
-	}
+int main(void)
+{
+	date d1;
+	cout << "Enter date 31/12/2015: ";
+	cin >> d1.day
+		>> d1.s
+		>> d1.month
+		>> d1.s
+		>> d1.year;
+	cout << endl;
+	
+	cout << d1.day << '/'
+		 << d1.month << '/'
+		 << d1.year;
+	cout << endl;
+	
+	return 0;
+}
