424ebf7bc37c5f79f9760e7a7a08fc890b1d4ca9
Dmitry Golgovsky <d.westcoast@aol.com>
2017-05-18 16:26:57 +0300

    add old sources

---
+++ unsorted_sources/crazy_crements.cpp
@@ -0,0 +1,14 @@
+/*
+ * Java developers laughting of you
+ * A+=++A+++++A++;
+ */
+
+#include <iostream>
+
+int main(void)
+{
+    int i = 10;
+    i+=++i+++++i++; 
+    std::cout << i << std::endl;
+    return 0;
+}
