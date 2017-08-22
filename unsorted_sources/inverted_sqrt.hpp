821c6264b6570f386d8322f1c72b2780dddf7084
Dmitry Golgovsky <d.westcoast@aol.com>
2017-08-22 16:46:47 +0300

    inv_sqrt

---
+++ unsorted_sources/inverted_sqrt.hpp
@@ -0,0 +1,9 @@
+float inv_sqrt(float x)
+{
+    float xhalf = 0.5f * x;
+    int i = *(int*)&x;  // представим биты float в виде целого числа
+    i = 0x5f3759df - (i >> 1);  // какого черта здесь происходит ?
+    x = *(float*)&i;
+    x = x * (1.5f - (xhalf * x * x));
+    return x;
+}
