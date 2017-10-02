c0fb5320d21a27b7a2bce8ef6b5272c517a628d0
Dmitry Golgovsky <d.westcoast@aol.com>
2017-10-02 20:02:07 +0300

    float type =)

---
+++ unsorted_sources/float_comparing.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    float f_value1 = 1.1;
+    cout << "f_value1 = 1.1 ";
+
+    if (f_value1 == 1.1) {
+        cout << "equal 1.1;" << endl;
+    } else {
+        cout << "not equal 1.1;" << endl;
+    }
+
+    cout << setprecision(10) << f_value1 << endl;
+
+    if (f_value1 < 1.1000001) {
+        cout << "lt 1.1000001;" << endl;
+    } else {
+        cout << "float type is suck;" << endl;
+    }
+
+    return 0;
+}
+
