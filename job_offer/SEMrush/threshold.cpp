bd5b875e8720054dde14fb83a841e1be4161353b
Dmitry Golgovsky <d.westcoast@aol.com>
2017-10-27 14:46:55 +0300

    some modifications

--- job_offer/SEMrush/threshold.cpp
+++ job_offer/SEMrush/threshold.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 /*
  * input:
  * “vnk2435kvabco8awkh125kjneytbcd12qjhb4acd123xmnbqwnw4t”
@@ -10,33 +9,34 @@
  * sequence of length = 5 found at haystack offset 38, needle offset 2
  */
 
-using std::cout;
-using std::endl;
+#include <iostream>
 
-void print_result(int sequence, int offset, int needle_offset)
+inline void
+print_result( unsigned int sequence
+            , unsigned int offset
+            , unsigned int needle_offset )
 {
-    cout << "sequence of length = " << sequence
+    std::cout << "sequence of length = " << sequence
          << " found at haystack offset " << offset
          << ", needle offset " << needle_offset
-         << endl;
+         << std::endl;
 }
 
-int comlen(char *p, char *q, int threshold, int &needle_offset)
+inline unsigned int
+compire( char const   *p
+       , char const   *q
+       , unsigned int &needle_offset )
 {
-    int sequence = 0;
+    unsigned int sequence = 0;
     needle_offset = 0;
     while (*p != '\0') {
         if (*q == *p) {
-            while ( (*p != '\0' || *q != '\0') && *p == *q) {
+            while (*p == *q && (*p != '\0' || *q != '\0')) {
                 sequence++;
                 p++;
                 q++;
             }
-            if (sequence < threshold)
-                return 0;
-            else {
-                return sequence;
-            }
+            return sequence;
         }
         needle_offset++;
         p++;
@@ -45,29 +45,25 @@ int comlen(char *p, char *q, int threshold, int &needle_offset)
 }
 
 inline void
-search( char const *haystack
-      , char const *needle
-      , int        threshold )
+search( char const         *haystack
+      , char const         *needle
+      , unsigned int const threshold )
 {
-    int sequence = 0;
-    int offset = 0;
-    int needle_offset = 0;
+    unsigned int sequence = 0;
+    unsigned int offset = 0;
+    unsigned int needle_offset = 0;
 
-    char *c_h = const_cast<char*>(haystack);
-    char *c_n = const_cast<char*>(needle);
-    while (*c_h != '\0') {
-        sequence = comlen(c_n, c_h, threshold, needle_offset);
-        if (sequence) {
+    while (*(haystack+offset) != '\0') {
+        sequence = compire(needle, haystack+offset, needle_offset);
+        if (sequence >= threshold) {
             print_result(sequence, offset, needle_offset);
-            int i = 0;
-            while((i <= sequence) && (*c_h!='\0')) {
-                c_h++;
+            unsigned int i = 0;
+            while((i <= sequence) && (*(haystack+offset)!='\0')) {
                 offset++;
                 i++;
             }
             continue;
         }
-        c_h++;
         offset++;
     }
 }
@@ -76,9 +72,8 @@ int main()
 {
     char const
         *haystack = "vnk2435kvabco8awkh125kjneytbcd12qjhb4acd123xmnbqwnw4t";
-    char const
-        *needle = "abcd1234";
-    int threshold = 3;
+    char const *needle = "abcd1234";
+    unsigned int const threshold = 3;
 
     search(haystack, needle, threshold);
 
