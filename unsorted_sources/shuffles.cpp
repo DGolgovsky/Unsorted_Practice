d2af61187806cf35b9298be4ade54be61aeb84a0
Dmitry Golgovsky <d.westcoast@aol.com>
2017-11-02 16:59:54 +0300

    move, tuple, shuffle

---
+++ unsorted_sources/shuffles.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::vector<T> shuffle(std::vector<T> const &array)
+{
+    size_t half = array.size() / 2;
+    std::vector<T> tmp_array(array.size());
+    for (size_t i = 0; i < half; ++i) {
+        tmp_array[i*2] = array[i+half];
+        tmp_array[i*2+1] = array[i];
+    }
+    return tmp_array;
+}
+
+int main()
+{
+    std::vector<int> array = {1,2,3,4,5,6,7,8,9,10};
+    std::cout << "Original:\n";
+    for (auto const &it : array)
+        std::cout << it << " ";
+
+    array = shuffle(array);
+
+    std::cout << "\nConverted:\n";
+    for (auto const &it : array)
+        std::cout << it << " ";
+
+    std::cout << std::endl;
+
+    return 0;
+}
