18ca08629393bdcd91e93c1fcde22781d3baa50e
Dmitry Golgovsky <d.westcoast@aol.com>
2017-11-03 14:29:53 +0300

    std::tuple

---
+++ cpp_part2_less/week_2/test_2.3.3.cpp
@@ -0,0 +1,23 @@
+#include <tuple>
+#include <utility>  // std::forward
+#include <iostream> // std::cout
+
+template<size_t I1,
+         size_t I2,
+         typename... Args,
+         typename A = typename std::tuple_element<I1, std::tuple<Args...>>::type,
+         typename B = typename std::tuple_element<I2, std::tuple<Args...>>::type >
+std::pair<A, B> to_pair(std::tuple<Args...>& tuple_)
+{
+   auto pair_ = std::make_pair(std::get<I1>(tuple_), std::get<I2>(tuple_));
+   return pair_;
+}
+
+int main()
+{
+	auto t = std::make_tuple(0, 3.5, "Hello");
+	std::pair<double, char const *> p = to_pair<1,2>(t); 
+	// p содержит 3.5 и "Hello"
+	std::cout << p.first << ", " << p.second << '\n';
+	return 0;
+}
