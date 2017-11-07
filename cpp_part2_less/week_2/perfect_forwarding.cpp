e14b0899902e1f41345bebc506944d947676e0f1
Dmitry Golgovsky <d.westcoast@aol.com>
2017-11-07 20:42:26 +0300

    variadic rvalues

--- cpp_part2_less/week_2/perfect_forwarding.cpp
+++ cpp_part2_less/week_2/perfect_forwarding.cpp
@@ -17,3 +17,47 @@ unique_ptr<T> make_unique(Arg && arg) {
             new T(std::forward<Arg>(arg)));
 }
 
+// std::forward for lvalue:
+{
+    X x;
+    auto p = make_unique<A>(x);     // Arg = X&
+
+    unique_ptr<A> make_unique(X& && arg) {
+        return unique_ptr<A>(new A(std::forward<X&>(arg)));
+    }
+
+    X& && forward(remove_reference<X&>::type& a) {
+        return static_cast<X& &&>(a);
+    }
+
+    // After bonding
+    unique_ptr<A> make_unique(X& arg) {
+        return unique_ptr<A>(new A(std::forward<X&>(arg)));
+    }
+
+    X& forward(X& a) {
+        return static_cast<X&>(a);
+    }
+}
+
+// std::forward for rvalue:
+{
+    auto p = make_unique<A>(X());       // Arg = X
+
+    unique_ptr<A> make_unique(X&& arg) {
+        return unique_ptr<A>(new A(std::forward<X>(arg)));
+    }
+
+    X&& forward(remove_reference<X>::type& a) {
+        return static_cast<X&&>(a);
+    }
+
+    // After bonding
+    unique_ptr<A> make_unique(X&& arg) {
+        return unique_ptr<A>(new A(std::forward<X>(arg)));
+    }
+
+    X&& forward(X& a) {
+        return static_cast<X&&>(a);
+    }
+}
--- cpp_part2_less/week_2/rval_checks.cpp
+++ cpp_part2_less/week_2/rval_checks.cpp
@@ -1,11 +1,17 @@
+/*
+Выберите функции, при вызове которых обязательно происходит перемещение объекта String (у класса String определены соответствующие перемещающие методы).
+
+Hint: помните, что в теле функции формальный параметр является lvalue вне зависимости от того, как он был передан в функцию.
+Answer: 3, 4, 5
+*/
 #include <iostream>
 
 struct A {
     A() = default;
-    A(const A&) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
-    A& operator=(const A&) { std::cout << __PRETTY_FUNCTION__ << std::endl; return *this; }
-    A(A&&) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
-    A& operator=(A&&) { std::cout << __PRETTY_FUNCTION__ << std::endl; return *this; }
+    A(const A&) { std::cout << "Copy constructor" << std::endl; }
+    A& operator=(const A&) { std::cout << "Copy operator=" << std::endl; return *this; }
+    A(A&&) { std::cout << "Move constructor" << std::endl; }
+    A& operator=(A&&) { std::cout << "Move operator=" << std::endl; return *this; }
 };
 
 A & fun1(A & s)
@@ -45,27 +51,46 @@ A fun7(A && s)
 
 int main() {
     std::cout << "fun1:\n";
-    A a,b;    
-    b = fun1(a);
-
+    A a;
+    A b = fun1(a);
+    fun1(a);
+    //b = fun1(A());
+    
     std::cout << "\nfun2:\n";
-    A c,d;    
-    d = fun2(c);
+    A c;
+    A d = fun2(c);
+    fun2(c);
+    //d = fun2(A());
+    
     std::cout << "\nfun3:\n";    
-    A e,f;    
-    f = fun3(e);
+    A e;
+    A f = fun3(e);
+    fun3(e);
+    //f = fun3(A());
+    
     std::cout << "\nfun4:\n";    
-    A g,h;    
+    A g;
+    A h = fun4(A());
     h = fun4(g);
+    fun4(A());
+    
     std::cout << "\nfun5:\n";    
-    A i,j;    
+    A i;
+    A j = fun5(A());
     j = fun5(i);
+    fun5(A());
+    
     std::cout << "\nfun6:\n";    
-    A k,l;    
-    //l = fun6(k);
+    A k;
+    A l = fun6(A());
+    l = fun6(std::move(k));
+    fun6(A());
+    
     std::cout << "\nfun7:\n";    
-    A m,n;    
-    //n = fun7(m);
+    A m;
+    A n = fun7(A());
+    n = fun7(std::move(m));
+    fun7(A());
     
     return 0;
 }
