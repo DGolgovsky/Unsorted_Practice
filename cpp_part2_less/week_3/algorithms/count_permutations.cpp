b86104de5cbdb03fbc1eb04daec4b76647856a0e
Dmitry Golgovsky <d.westcoast@aol.com>
2017-11-16 19:45:38 +0300

    fix count_permutations

--- cpp_part2_less/week_3/algorithms/count_permutations.cpp
+++ cpp_part2_less/week_3/algorithms/count_permutations.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 #include <algorithm>
-
+/*
 template<class Iterator>
 size_t count_permutations(Iterator p1, Iterator q1)
 {
@@ -18,6 +18,29 @@ size_t count_permutations(Iterator p1, Iterator q1)
             count++;
     } while (std::next_permutation(p, q));
     return count;
+}
+*/
+/* Better solution */
+template <typename Iterator>
+std::size_t count_permutations(Iterator p, Iterator q)
+{
+    using ItType = typename std::iterator_traits<Iterator>::value_type;
+
+    if (p == q) {
+        return 1;
+    }
+
+    std::vector<ItType> v(p, q);
+    std::sort(v.begin(), v.end());
+    std::size_t perm_count = (std::adjacent_find(v.cbegin(), v.cend()) == v.cend());
+
+    while (std::next_permutation(v.begin(), v.end())) {
+        if (std::adjacent_find(v.cbegin(), v.cend()) == v.cend()) {
+            ++perm_count;
+        }
+    }
+
+    return perm_count;
 }
 
 int main()
