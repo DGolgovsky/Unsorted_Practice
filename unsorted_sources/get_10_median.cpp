92e9ea04c4f0ebf2385b78e949feccd4dba9672c
Dmitry Golgovsky <d.westcoast@aol.com>
2017-12-04 13:29:16 +0300

    example algo-sorts

---
+++ unsorted_sources/get_10_median.cpp
@@ -0,0 +1,22 @@
+/*
+ * Получить сортированный список из 10 человек
+ * с балансом на счету близким к медиане.
+ *
+ * => отсортировать всех по балансу и выбрать 10 человек из середины
+ */
+#include <algorithm>
+
+template <typename It>
+auto balance_median(It const &v) // ~O(N*log(N))
+{
+    auto it = v.begin() + v.size() / 2 - 5;
+
+    const auto f = [](const auto& v1, const auto& v2) {
+        return v1.balance() < v2.balance();
+    };
+
+    std::nth_element(v.begin(), it, v.end(), f);
+    std::partial_sort(it + 1, it + 10, v.end(), f);
+
+    return it;
+}
