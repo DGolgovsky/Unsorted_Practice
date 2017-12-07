656cbd365f8c12493f4e68fb44aaf6daa6d405e9
Dmitry Golgovsky <d.westcoast@aol.com>
2017-12-07 13:28:01 +0300

    Pascal triangular C program

---
+++ unsorted_sources/pascal_trg.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+	int n = 0;
+	int a[20] = {1};
+	
+	scanf("%d", &n);
+	for(i = 1; i <= n; ++i) {
+		for(k = i - 1; k; --k) {
+			a[k] = a[k] + a[k - 1];
+		}
+		a[i - 1] = 1;
+		
+		for(l = 0; l < n - i; ++l)
+			printf(" ");
+		for(l = 0; l < i; ++l)
+			printf("%d ", a[l]);
+		printf("\n");
+	}
+	
+	return 0;
+}
