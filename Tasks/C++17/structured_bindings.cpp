5aebe87ceb0eb040bba1e38c7747e6ae9511786a
Dmitry Golgovsky <d.westcoast@aol.com>
2017-12-07 13:27:25 +0300

    C++17 feautures

---
+++ Tasks/C++17/structured_bindings.cpp
@@ -0,0 +1,52 @@
+#include <tuple>
+#include <string>
+#include <map>
+#include <cassert>
+#include <iostream>
+
+struct BookInfo
+{
+    std::string title; // In UTF-8
+    int yearPublished = 0;
+};
+
+template <class T>
+T readBookInfo(T &t)
+{
+    return t;
+}
+
+int main()
+{
+    using namespace std::literals;
+
+    BookInfo book = {"Advanced C++", 2017};
+    // Раскладываем поля структуры на переменные title и year, тип которых выведен автоматически
+    auto [title, year] = readBookInfo(book);
+
+    auto [a, b, c] = std::tuple<int,std::string,double>(42, "hello"s, 3.14);
+
+    std::cout << title << ": " << year << '\n';
+    std::cout << a << ", " << b << ", " << c << '\n';
+
+    /*
+     * Пример декомпозиции с try_emplace и
+     * декомпозиции key-value при обходе map:
+     */
+    std::map<std::string, std::string> map;
+    auto [iterator1, succeed1] = map.try_emplace("key", "abc");
+    auto [iterator2, succeed2] = map.try_emplace("key", "cde");
+    auto [iterator3, succeed3] = map.try_emplace("another_key", "cde");
+
+    assert(succeed1);
+    assert(!succeed2);
+    assert(succeed3);
+
+    // Вы можете раскладывать key и value прямо в range-based for
+    for (auto&& [key, value] : map)
+    {
+        std::cout << key << ": " << value << "\n";
+    }
+
+    return 0;
+}
---
+++ unsorted_sources/osyncstream_thread.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+#include <string_view>
+
+void say_hello(std::string_view username) {
+    std::osyncstream{std::cerr} << "Hello " << username;
+}
+
+void beware_of(std::string_view something) {
+    std::osyncstream(std::cerr) << something << " is dangerous";
+}
+
+int main() {
+    std::thread t1([]{
+        say_hello("Rachel");
+    });
+
+    std::thread t2([]{
+        beware_of("darkness");
+    });
+
+    std::cerr << '\n';
+ 
+    t2.join();
+    t1.join();
+}
