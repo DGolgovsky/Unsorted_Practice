e552bb38b74f128d798c9d07c8cf31ae5a2112c5
Dmitry Golgovsky <d.westcoast@aol.com>
2017-12-18 12:51:32 +0300

    Max number of consecutive 1's in n's representation

---
+++ unsorted_sources/count_base-2_ones.cpp
@@ -0,0 +1,27 @@
+/*
+ * Given a base-10 integer, n, convert it to binary (base-2).
+ * Then find and print the base-10 integer denoting
+ * the maximum number of consecutive 1's in n's binary representation.
+ */
+
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int n = 0;
+    std::cin >> n;
+    
+    int counter = 0, max =0;
+    while (n > 0) {
+        int rem = n % 2;
+        if (rem == 1)
+            counter++;
+        else
+            counter = 0;
+        max = std::max(counter, max);
+        n /= 2;
+    }
+    std::cout << max << '\n';
+    
+    return 0;
+}
