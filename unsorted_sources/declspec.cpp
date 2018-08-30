2aee0223076c13fbfea34f2791ef6926bfc524e0
Dmitry Golgovsky <d.westcoast@aol.com>
2018-08-30 10:20:55 +0300

    Add some scrap

---
+++ unsorted_sources/declspec.cpp
@@ -0,0 +1,30 @@
+// clang -fdeclspec -lstdc++ this.cpp -o a.out
+
+#include <iostream>
+
+class Person
+{
+private:
+    int age_;
+
+public:
+    int get_age() const
+    {
+        return age_;
+    }
+    void set_age(int value)
+    {
+        age_ = value;
+    }
+    __declspec(property(get=get_age, put=set_age)) int age;
+};
+
+int main()
+{
+    Person person;
+    person.age = 20;
+
+    std::cout << person.age << '\n';
+
+    return 0;
+}
